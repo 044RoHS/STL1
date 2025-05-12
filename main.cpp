1fa07c1bf7c4d1470cd140fe864c0469158f856f
Artem <70117318+044RoHS@users.noreply.github.com>
2025-05-12 14:10:54 +0300

    Second commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include<vector>
+#include<algorithm>
+
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int count;
+    cout << "Введите количесвто элементов";
+    cin >> count ;
+    std::vector<int>vec(count);
+    for (int i = 0 ; i < count; ++i){
+        cin >> vec[i];
+    }
+    std::sort(vec.begin(),vec.end());
+    auto it = std::unique(vec.begin(),vec.end());
+    vec.erase(it ,vec.end());
+    for ( auto i = vec.rbegin(); i != vec.rend(); ++i){
+        cout << *i << endl;
+    }
     return 0;
 }
